a195d18fca272c604e27944252071fb02f64ba5b
mufolk <guilhermemelo138@gmail.com>
2023-05-31 15:28:11 -0300

    feat: adding the fonts, colors, themes and also the navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import NavBar from "@/component/NavBar";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Gui.Dev</title>
+      </Head>
+      <main
+        className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
+      >
+        <NavBar />
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
